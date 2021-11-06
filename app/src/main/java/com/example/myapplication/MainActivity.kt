58aead6f5b2649a6feb2a76d40737b6ba66db3e0
alemlishar <alextopten03@gmail.com>
2021-11-07 00:30:45 +0100

    second commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.LENGTH_SHORT;
+    }
+    
+}
